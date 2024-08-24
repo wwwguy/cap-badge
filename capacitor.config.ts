b1c33d0434f0ff009f08f51d55ceb88e1893d8ce
Mike Darling <mike@Mikes-iMac.local>
2024-08-24 09:37:08 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.capbadge.com',
+  appName: 'CapBadge',
+  webDir: 'www'
+};
+
+export default config;
